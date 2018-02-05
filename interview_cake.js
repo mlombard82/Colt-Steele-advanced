385413bde18dfe1fa44093d062001904df63376b
Mau Lomba <mauriciolombard@liamdeMacBook-Air.local>
2018-02-04 18:25:46 -0600

    apple stock solution

--- interview_cake.js
+++ interview_cake.js
@@ -17,9 +17,47 @@
 
 // No "shorting"—you must buy before you sell. You may not buy and sell in the same time step (at least 1 minute must pass).
 
+// INITIAL ATTEMPTS:
+// BRUTE FORCE:O(N^2) nested loops. if we are going to do better than O(N^2), probably gonna be O(n lgn) or O(N). O(n lgn) comes up in SORTING/SEARCHING ALGO where we are recursively cutting the array in half. not useful here.
+
 // SOLUTION. we'll greedily walk through the array to track the max profit and lowest price so far. A greedy algo builds up a solution by choosing the option that looks the best at every step.
 // To try it on a new problem, start by asking yourself:
 // "Suppose we could come up with the answer in one pass through the input, by simply updating the 'best answer so far' as we went. What additional values would we need to keep updated as we looked at each item in our input, in order to be able to update the 'best answer so far' in constant time?"
+
+function getMaxProfit(stockPricesYesterday) {
+
+    if (stockPricesYesterday.length < 2) {
+        throw new Error('Getting a profit requires at least 2 prices');
+    }
+
+    // we'll greedily update minPrice and maxProfit, so we initialize
+    // them to the first price and the first possible profit
+    var minPrice = stockPricesYesterday[0];
+    var maxProfit = stockPricesYesterday[1] - stockPricesYesterday[0];
+
+    // start at the second (index 1) time
+    // we can't sell at the first time, since we must buy first,
+    // and we can't buy and sell at the same time!
+    // if we started at index 0, we'd try to buy *and* sell at time 0.
+    // this would give a profit of 0, which is a problem if our
+    // maxProfit is supposed to be *negative*--we'd return 0.
+    for (var i = 1; i < stockPricesYesterday.length; i++) {
+        var currentPrice = stockPricesYesterday[i];
+
+        // see what our profit would be if we bought at the
+        // min price and sold at the current price
+        var potentialProfit = currentPrice - minPrice;
+
+        // update maxProfit if we can do better
+        maxProfit = Math.max(maxProfit, potentialProfit);
+
+        // update minPrice so it's always
+        // the lowest price we've seen so far
+        minPrice = Math.min(minPrice, currentPrice);
+    }
+
+    return maxProfit;
+}
 
 
 
